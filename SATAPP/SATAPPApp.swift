216910e5295923c35c33ca09cd1b1d1470af8362
Rachel Mills <rmills30@gatech.edu>
2024-01-07 17:12:59 -0500

    Initial Commit

---
+++ SATAPP/SATAPPApp.swift
@@ -0,0 +1,20 @@
+//
+//  SATAPPApp.swift
+//  SATAPP
+//
+//  Created by Rachel Mills on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SATAPPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

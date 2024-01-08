005ddb836bb56729ab1a8091f645ccaf48e0b76d
Rachel Mills <rmills30@gatech.edu>
2024-01-08 17:20:11 -0500

    first practice page and small features

---
+++ SATAPP/PracticeQuestionView.swift
@@ -0,0 +1,87 @@
+//
+//  PracticeQuestionView.swift
+//  SATAPP
+//
+//  Created by Rachel Mills on 1/8/24.
+//
+
+import SwiftUI
+
+struct PracticeQuestionView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Question 1")
+                    .padding()
+                
+                Spacer()
+                Button("Test") {
+                    
+                }
+                .buttonStyle(PracticeAnswerButton())
+                Button("Test") {
+                    
+                }
+                .buttonStyle(PracticeAnswerButton())
+                Button("Test") {
+                    
+                }
+                .buttonStyle(PracticeAnswerButton())
+                Button("Test") {
+                    
+                }
+                .buttonStyle(PracticeAnswerButton())
+                Button("Test") {
+                    
+                }
+                .buttonStyle(PracticeAnswerButton())
+                .padding(.bottom, 40)
+                
+                
+            }
+            VStack {
+                HStack {
+                    Spacer()
+                    ZStack {
+                        Image(systemName: "chevron.backward")
+                            .padding(.trailing, 50)
+                        Image(systemName: "doc")
+                            .font(.system(size: 25))
+                    }.frame(width: 20, height: 50, alignment: .trailing)
+                    
+                }
+                .padding(.top, 70)
+           
+                Spacer()
+            }
+           
+        }
+    }
+}
+
+struct PracticeAnswerButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(width: 310, height: 40)
+            .font(.system(size: 22))
+            .background(Color(red: 1, green: 1, blue: 1))
+            .cornerRadius(6)
+            .foregroundStyle(.black)
+            //.shadow(color: (Color(UIColor.lightGray)), radius: 15)
+            .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(.gray, lineWidth: 0.75)
+                        
+                )
+            .scaleEffect(configuration.isPressed ? 1.05 : 1)
+            .animation(.easeOut(duration: 0.3), value: configuration.isPressed)
+            .padding([.top, .bottom], 1)
+    }
+}
+
+struct PracticeQuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PracticeQuestionView()
+    }
+}
